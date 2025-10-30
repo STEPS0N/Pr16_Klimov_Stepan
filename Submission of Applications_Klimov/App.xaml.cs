8dd4eb0d1aa008da094857b431fb76e6eb33eb33
Степан Климов <stepan.chik228@mail.ru>
2025-10-30 14:20:58 +0500

    Добавьте файлы проекта.

---
+++ Submission of Applications_Klimov/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Submission_of_Applications_Klimov
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
